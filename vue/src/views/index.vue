65cdcc048a73bab29d90a2d6661fc39b9a764257
unknown <1349541630@qq.com>
2020-04-24 14:13:05 +0800

    first

---
+++ vue/src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import admin from './admin'
+import editor from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: { admin, editor },
+  data() {
+    return {
+      currentRole: 'admin'
+    }
+  },
+}
+</script>
