7eb759b2e07147cd9ef1c411343134217bf79dc8
unknown <1349541630@qq.com>
2020-04-28 14:58:55 +0800

    增加浏览器顶部进度条nprogress

--- vue/src/main.js
+++ vue/src/main.js
@@ -6,6 +6,16 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/common.css';
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+router.afterEach(() => {
+  NProgress.done()
+})
 Vue.config.productionTip = false
 router.push('/index')
 Vue.use(ElementUI);
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -7,17 +7,16 @@
     text-color="rgb(191, 203, 217)"
     router
     unique-opened
-    collapse-transition
     active-text-color="rgb(64, 158, 255)"
     :default-active="$route.path"
-    @open="handleOpen"
-    @close="handleClose">
+    @open="handleOpen" 
+    @close="handleClose" mode="vertical">
         <template v-for="(item,index) in $router.options.routes" v-key='item.path'> 
             <el-submenu :index="index+''" v-if="!item.leaf">
                 <template slot="title">
                     <i :class="'iconfont '+item.icon"></i>
                     <span>{{item.name}}</span>
-                    </template>
+                </template>
                 <el-menu-item-group>
                     <el-menu-item  v-for='(child,index) in item.children' :index='child.path' :key='index'>
                         <template slot="title">
@@ -82,27 +81,28 @@ export default {
                 padding:0 15px;
             }
         }
-        .el-menu {
-            height:100%;
-        }
-        .el-submenu .el-menu-item {
-            min-width: 150px;
-        }
-        .el-menu-vertical-demo:not(.el-menu--collapse) {
-            width: 200px;
-        }
-        .el-menu-item i,.el-submenu__title i {
-            margin-right:10px;
-        }
-        // menu hover
-        .submenu-title-noDropdown:hover,.el-submenu__title:hover {
-            background-color: #263445!important;
-        }
-        .is-active>.el-submenu__title {
-        color: #f4f4f5!important;
-        }
-    .nest-menu .el-submenu>.el-submenu__title:hover,.el-submenu .el-menu-item:hover,.el-menu-item:hover {
-            background-color: #263445!important;
-        }
+        
+    }
+    .el-menu {
+        height:100%;
+    }
+    .el-submenu .el-menu-item {
+        min-width: 150px;
+    }
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+        width: 200px;
+    }
+    .el-menu-item i,.el-submenu__title i {
+        margin-right:10px;
+    }
+    // menu hover
+    .submenu-title-noDropdown:hover,.el-submenu__title:hover,.el-menu-item:focus, .el-menu-item:hover {
+        background-color: #263445!important;
+    }
+    .is-active>.el-submenu__title {
+    color: #f4f4f5!important;
+    }
+    .el-menu--vertical .nest-menu .el-submenu > .el-submenu__title:hover, .el-menu--vertical .el-menu-item:hover,.el-submenu__title:hover,.el-submenu__title:focus {
+        background-color: #263445!important;
     }
 </style>
