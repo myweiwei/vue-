fc1bf8b2780377c3ef3494e258bacb33e001a0f6
unknown <1349541630@qq.com>
2020-04-28 10:56:38 +0800

    增加导航栏展开收起功能

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,40 +1,44 @@
 <template>
-  <div class='home'>
-    <el-row justify="center">
-        <el-col :span='3'>
-            <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            background-color="#545c64"
-            text-color="rgb(191, 203, 217)"
-            router
-            unique-opened
-            active-text-color="rgb(64, 158, 255)"
-            :default-active="$route.path"
-            @open="handleOpen"
-            @close="handleClose">
-                <template v-for="(item,index) in $router.options.routes" v-key='item.path'> 
-                    <el-submenu :index="index+''" v-if="!item.leaf">
-                        <template slot="title"><i :class="'iconfont '+item.icon"></i>{{item.name}}</template>
-                        <el-menu-item  v-for='(child,index) in item.children' :index='child.path' :key='index'>
-                            <template slot="title">
-                                <span>{{child.name}}</span>
-                            </template>
-                        </el-menu-item>
-                    </el-submenu>
-                    <el-menu-item :index='item.children[0].path' v-if="item.leaf">
+  <div class='home flexStartBox'>
+    <el-menu
+    :collapse="isCollapse"
+    class="el-menu-vertical-demo"
+    background-color="rgb(48, 65, 86)"
+    text-color="rgb(191, 203, 217)"
+    router
+    unique-opened
+    collapse-transition
+    active-text-color="rgb(64, 158, 255)"
+    :default-active="$route.path"
+    @open="handleOpen"
+    @close="handleClose">
+        <template v-for="(item,index) in $router.options.routes" v-key='item.path'> 
+            <el-submenu :index="index+''" v-if="!item.leaf">
+                <template slot="title">
+                    <i :class="'iconfont '+item.icon"></i>
+                    <span>{{item.name}}</span>
+                    </template>
+                <el-menu-item-group>
+                    <el-menu-item  v-for='(child,index) in item.children' :index='child.path' :key='index'>
                         <template slot="title">
-                            <i :class="'iconfont '+item.icon"></i>
-                            <span>{{item.name}}</span>
+                            <span>{{child.name}}</span>
                         </template>
                     </el-menu-item>
-                </template>
-            </el-menu>
-        </el-col>
-        <el-col :span='21'>
-            <router-view></router-view>
-        </el-col>
-    </el-row>
+                </el-menu-item-group>
+            </el-submenu>
+            <el-menu-item :index='item.children[0].path' v-if="item.leaf">
+                <i :class="'iconfont '+item.icon"></i>
+                <span>{{item.name}}</span>
+            </el-menu-item>
+        </template>
+    </el-menu>
+    <div style='flex:1;height:100%'>
+        <div class="navBar">
+            <i class='iconfont icondaohangshouqi- pointer' @click='isCollapse=true' v-if='!isCollapse'></i>
+            <i class='iconfont iconzhankai  pointer' @click='isCollapse=false' v-if='isCollapse'></i>
+        </div>
+        <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -43,7 +47,8 @@ export default {
   name: '',
   data(){
     return {
-        navTitle:""
+        navTitle:"",
+        isCollapse:false
     }
   },
   mounted(){
@@ -62,19 +67,42 @@ export default {
 </script>
 
 <style lang='scss'>
-    .el-submenu .el-menu-item {
-        min-width: 150px;
-    }
     .home {
         height:100%;
-        .el-row {
-            height:100%;
-            .el-col,.el-menu {
-                height:100%;
+        .navBar {
+            height: 50px;
+            overflow: hidden;
+            position: relative;
+            background: #fff;
+            -webkit-box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+            box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+            line-height: 50px;
+            i {
+                font-size: 20px;
+                padding:0 15px;
             }
         }
-    }
-    .el-menu-item i,.el-submenu__title i {
-        margin-right:10px;
+        .el-menu {
+            height:100%;
+        }
+        .el-submenu .el-menu-item {
+            min-width: 150px;
+        }
+        .el-menu-vertical-demo:not(.el-menu--collapse) {
+            width: 200px;
+        }
+        .el-menu-item i,.el-submenu__title i {
+            margin-right:10px;
+        }
+        // menu hover
+        .submenu-title-noDropdown:hover,.el-submenu__title:hover {
+            background-color: #263445!important;
+        }
+        .is-active>.el-submenu__title {
+        color: #f4f4f5!important;
+        }
+    .nest-menu .el-submenu>.el-submenu__title:hover,.el-submenu .el-menu-item:hover,.el-menu-item:hover {
+            background-color: #263445!important;
+        }
     }
 </style>
